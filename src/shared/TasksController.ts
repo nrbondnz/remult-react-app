56c368181a9d4f214ff01ff1a965e8eb5005fef2
Bond in Auckland <nigel.bond@orcon.net.nz>
2024-09-07 23:28:25 +1200

    adding in graphql, not finished yet

--- src/shared/TasksController.ts
+++ src/shared/TasksController.ts
@@ -1,7 +1,7 @@
 ﻿// src/shared/TasksController.ts
 
 import { BackendMethod, remult } from 'remult'
-import { Task } from './model/Task.ts'
+import { Task } from './model/Task'
 
 export class TasksController {
     @BackendMethod({ allowed: true })
